2fe0e5ae7822b0d9546efc9695f821fb6aa08134
Mykhailo Smolinskyi <nntndfrk@gmail.com>
2026-01-15 19:15:43 +0200

    refactor: use NestJS Logger instead of console.log

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
@@ -6,16 +7,14 @@ import { type AppConfig } from './config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
   const configService = app.get(ConfigService);
   const appConfig = configService.getOrThrow<AppConfig>('app');
 
   await app.listen(appConfig.port);
 
-  if (appConfig.isDevelopment) {
-    //eslint-disable-next-line no-console
-    console.log('Server is running on port', appConfig.port);
-  }
+  logger.log(`Server is running on port ${String(appConfig.port)}`);
 }
 
 void bootstrap();
